6104d571207de3120b4164d60c393bd7082378c4
mr-augustine <pushbuttonrobotics@gmail.com>
2015-09-24 12:42:50 -0500

    Expanded Signup skill areas

--- lab2/ha_lab2/WebContent/views/SignupView.class.php
+++ lab2/ha_lab2/WebContent/views/SignupView.class.php
@@ -26,16 +26,25 @@ class SignupView {
 			
 			<fieldset>
 				<legend>Skill Areas</legend>
-				<input type="checkbox" name="skill_area" value="sensors" tabindex="7">sensors	
-				<input type="checkbox" name="skill_area" value="code" tabindex="8">code	
-				<input type="checkbox" name="skill_area" value="mechanisms" tabindex="9">mechanisms
+				<input type="checkbox" name="skill_area" value="system-design" tabindex="7">system-design
+				<input type="checkbox" name="skill_area" value="programming" tabindex="8">programming
+				<input type="checkbox" name="skill_area" value="machining" tabindex="9">mechanining
+				<input type="checkbox" name="skill_area" value="soldering" tabindex="10">soldering
+				<input type="checkbox" name="skill_area" value="wiring" tabindex="11">wiring
+				<input type="checkbox" name="skill_area" value="circuit-design" tabindex="12">circuit-design
+				<input type="checkbox" name="skill_area" value="power-systems" tabindex="13">power-systems
+				<input type="checkbox" name="skill_area" value="computer-vision" tabindex="14">computer-vision
+				<input type="checkbox" name="skill_area" value="ultrasonic" tabindex="15">ultrasonic
+				<input type="checkbox" name="skill_area" value="infrared" tabindex="16">infrared
+				<input type="checkbox" name="skill_area" value="GPS" tabindex="17">GPS
+				<input type="checkbox" name="skill_area" value="compass" tabindex="18">compass
 			</fieldset><br>
 		
-			Profile Picture: <input type="file" id="fileinput" accept="image/*" tabindex="10"><br><br>
-			Started Robotics: <input type="month" id="first_hobby" tabindex="11"><br><br>
-			Favorite Color: <input type="color" name="fav_color" tabindex="12"><br><br>
-			URL: <input type="url" name="url" tabindex="13"><br><br>
-			Telephone: <input type="tel" name="phone" tabindex="14"><br><br>
+			Profile Picture: <input type="file" id="fileinput" accept="image/*" tabindex="19"><br><br>
+			Started Robotics: <input type="month" id="first_hobby" tabindex="20"><br><br>
+			Favorite Color: <input type="color" name="fav_color" tabindex="21"><br><br>
+			URL: <input type="url" name="url" tabindex="22"><br><br>
+			Telephone: <input type="tel" name="phone" tabindex="23"><br><br>
 			
 			<input type="submit" value="Submit">
 		</form>
