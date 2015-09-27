84cfb8434ab75ae4f889338eb7e1909376247862
mr-augustine <pushbuttonrobotics@gmail.com>
2015-09-27 14:03:49 -0500

    Updated skill areas selection

--- lab2/ha_lab2/WebContent/controllers/SignupController.class.php
+++ lab2/ha_lab2/WebContent/controllers/SignupController.class.php
@@ -2,13 +2,14 @@
 class SignupController {
 
 	public static function run() {
-// 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			simpleEcho.php;
 // 			$user = new User($_POST);
 // 			if ($user->getErrorCount() == 0)
 // 				HomeView::show();
 // 			else
 // 				SignupView::show($user);
-// 		} else  // Initial link
+		} else  // Initial link
 			SignupView::show(null);
 	}
 }
--- lab2/ha_lab2/WebContent/views/SignupView.class.php
+++ lab2/ha_lab2/WebContent/views/SignupView.class.php
@@ -26,22 +26,22 @@ class SignupView {
 			
 			<fieldset>
 				<legend>Skill Areas</legend>
-				<input type="checkbox" name="skill_area" value="system-design" tabindex="7">system-design
-				<input type="checkbox" name="skill_area" value="programming" tabindex="8">programming
-				<input type="checkbox" name="skill_area" value="machining" tabindex="9">mechanining
-				<input type="checkbox" name="skill_area" value="soldering" tabindex="10">soldering
-				<input type="checkbox" name="skill_area" value="wiring" tabindex="11">wiring
-				<input type="checkbox" name="skill_area" value="circuit-design" tabindex="12">circuit-design
-				<input type="checkbox" name="skill_area" value="power-systems" tabindex="13">power-systems
-				<input type="checkbox" name="skill_area" value="computer-vision" tabindex="14">computer-vision
-				<input type="checkbox" name="skill_area" value="ultrasonic" tabindex="15">ultrasonic
-				<input type="checkbox" name="skill_area" value="infrared" tabindex="16">infrared
-				<input type="checkbox" name="skill_area" value="GPS" tabindex="17">GPS
-				<input type="checkbox" name="skill_area" value="compass" tabindex="18">compass
+				<input type="checkbox" name="skill_areas[]" value="system-design" tabindex="7">system-design
+				<input type="checkbox" name="skill_areas[]" value="programming" tabindex="8">programming
+				<input type="checkbox" name="skill_areas[]" value="machining" tabindex="9">machining
+				<input type="checkbox" name="skill_areas[]" value="soldering" tabindex="10">soldering
+				<input type="checkbox" name="skill_areas[]" value="wiring" tabindex="11">wiring
+				<input type="checkbox" name="skill_areas[]" value="circuit-design" tabindex="12">circuit-design
+				<input type="checkbox" name="skill_areas[]" value="power-systems" tabindex="13">power-systems
+				<input type="checkbox" name="skill_areas[]" value="computer-vision" tabindex="14">computer-vision
+				<input type="checkbox" name="skill_areas[]" value="ultrasonic" tabindex="15">ultrasonic
+				<input type="checkbox" name="skill_areas[]" value="infrared" tabindex="16">infrared
+				<input type="checkbox" name="skill_areas[]" value="GPS" tabindex="17">GPS
+				<input type="checkbox" name="skill_areas[]" value="compass" tabindex="18">compass
 			</fieldset><br>
 		
-			Profile Picture: <input type="file" id="fileinput" accept="image/*" tabindex="19"><br><br>
-			Started Robotics: <input type="month" id="first_hobby" tabindex="20"><br><br>
+			Profile Picture: <input type="file" name="fileinput" accept="image/*" tabindex="19"><br><br>
+			Started Robotics: <input type="month" name="first_hobby" min="1970-01" max="<?php echo date("Y-m")?>" tabindex="20"><br><br>
 			Favorite Color: <input type="color" name="fav_color" tabindex="21"><br><br>
 			URL: <input type="url" name="url" tabindex="22"><br><br>
 			Telephone: <input type="tel" name="phone" tabindex="23"><br><br>
