a16ff67c1ee6c70d8f5f4606c556ca3a53ef16fd
mr-augustine <pushbuttonrobotics@gmail.com>
2015-09-28 19:53:58 -0500

    Messages tests complete

--- lab2/ha_lab2/WebContent/tests/Messages_tests.php
+++ lab2/ha_lab2/WebContent/tests/Messages_tests.php
@@ -3,7 +3,7 @@
 <head>
 </head>
 <body>
-<h1>UserData tests</h1>
+<h1>Messages tests</h1>
 
 <?php
 include_once("../models/Messages.class.php");
@@ -18,7 +18,21 @@ echo "EMAIL_EMPTY: " .Messages::getError("EMAIL_EMPTY")."<br>";
 echo "EMAIL_INVALID: " .Messages::getError("EMAIL_INVALID")."<br>";
 echo "USER_NAME_EMPTY: " .Messages::getError("USER_NAME_EMPTY")."<br>";
 echo "USER_NAME_TOO_SHORT: " .Messages::getError("USER_NAME_TOO_SHORT")."<br>";
-echo "USER_NAME_INVALID: " .Messages::getError("USER_NAME_HAS_INVALID_CHARS")."<br>";
+echo "USER_NAME_HAS_INVALID_CHARS: " .Messages::getError("USER_NAME_HAS_INVALID_CHARS")."<br>";
+echo "SKILL_AREA_INVALID: " .Messages::getError("SKILL_AREA_INVALID")."<br>";
+echo "SKILL_LEVEL_INVALID: " .Messages::getError("SKILL_LEVEL_INVALID")."<br>";
+echo "SKILL_LEVEL_NOT_SET: " .Messages::getError("SKILL_LEVEL_NOT_SET")."<br>";
+echo "PASSWORD_EMPTY: " .Messages::getError("PASSWORD_EMPTY")."<br>";
+echo "PASSWORD_TOO_SHORT: " .Messages::getError("PASSWORD_TOO_SHORT")."<br>";
+echo "PROFILE_PIC_WRONG_TYPE: " .Messages::getError("PROFILE_PIC_WRONG_TYPE")."<br>";
+echo "HOBBY_DATE_EMPTY: " .Messages::getError("HOBBY_DATE_EMPTY")."<br>";
+echo "HOBBY_DATE_FORMAT_INVALID: " .Messages::getError("HOBBY_DATE_FORMAT_INVALID")."<br>";
+echo "HOBBY_DATE_INVALID: " .Messages::getError("HOBBY_DATE_INVALID")."<br>";
+echo "FAV_COLOR_EMPTY: " .Messages::getError("FAV_COLOR_EMPTY")."<br>";
+echo "FAV_COLOR_INVALID: " .Messages::getError("FAV_COLOR_INVALID")."<br>";
+echo "URL_INVALID: " .Messages::getError("URL_INVALID")."<br>";
+echo "PHONE_NUMBER_INVALID: " .Messages::getError("PHONE_NUMBER_INVALID")."<br>";
+
 
 echo (empty(Messages::getError("USER_NAME_TOO_SHORT")))?
       "Failed: it did not set USER_NAME_TOO_SHORT from file":"";
@@ -32,7 +46,20 @@ echo "EMAIL_EMPTY: " .Messages::getError("EMAIL_EMPTY")."<br>";
 echo "EMAIL_INVALID: " .Messages::getError("EMAIL_INVALID")."<br>";
 echo "USER_NAME_EMPTY: " .Messages::getError("USER_NAME_EMPTY")."<br>";
 echo "USER_NAME_TOO_SHORT: " .Messages::getError("USER_NAME_TOO_SHORT")."<br>";
-echo "USER_NAME_INVALID: " .Messages::getError("USER_NAME_HAS_INVALID_CHARS")."<br>";
+echo "USER_NAME_HAS_INVALID_CHARS: " .Messages::getError("USER_NAME_HAS_INVALID_CHARS")."<br>";
+echo "SKILL_AREA_INVALID: " .Messages::getError("SKILL_AREA_INVALID")."<br>";
+echo "SKILL_LEVEL_INVALID: " .Messages::getError("SKILL_LEVEL_INVALID")."<br>";
+echo "SKILL_LEVEL_NOT_SET: " .Messages::getError("SKILL_LEVEL_NOT_SET")."<br>";
+echo "PASSWORD_EMPTY: " .Messages::getError("PASSWORD_EMPTY")."<br>";
+echo "PASSWORD_TOO_SHORT: " .Messages::getError("PASSWORD_TOO_SHORT")."<br>";
+echo "PROFILE_PIC_WRONG_TYPE: " .Messages::getError("PROFILE_PIC_WRONG_TYPE")."<br>";
+echo "HOBBY_DATE_EMPTY: " .Messages::getError("HOBBY_DATE_EMPTY")."<br>";
+echo "HOBBY_DATE_FORMAT_INVALID: " .Messages::getError("HOBBY_DATE_FORMAT_INVALID")."<br>";
+echo "HOBBY_DATE_INVALID: " .Messages::getError("HOBBY_DATE_INVALID")."<br>";
+echo "FAV_COLOR_EMPTY: " .Messages::getError("FAV_COLOR_EMPTY")."<br>";
+echo "FAV_COLOR_INVALID: " .Messages::getError("FAV_COLOR_INVALID")."<br>";
+echo "URL_INVALID: " .Messages::getError("URL_INVALID")."<br>";
+echo "PHONE_NUMBER_INVALID: " .Messages::getError("PHONE_NUMBER_INVALID")."<br>";
 
 ?>
 
@@ -45,7 +72,20 @@ echo "EMAIL_EMPTY: " .Messages::getError("EMAIL_EMPTY")."<br>";
 echo "EMAIL_INVALID: " .Messages::getError("EMAIL_INVALID")."<br>";
 echo "USER_NAME_EMPTY: " .Messages::getError("USER_NAME_EMPTY")."<br>";
 echo "USER_NAME_TOO_SHORT: " .Messages::getError("USER_NAME_TOO_SHORT")."<br>";
-echo "USER_NAME_INVALID: " .Messages::getError("USER_NAME_HAS_INVALID_CHARS")."<br>";
+echo "USER_NAME_HAS_INVALID_CHARS: " .Messages::getError("USER_NAME_HAS_INVALID_CHARS")."<br>";
+echo "SKILL_AREA_INVALID: " .Messages::getError("SKILL_AREA_INVALID")."<br>";
+echo "SKILL_LEVEL_INVALID: " .Messages::getError("SKILL_LEVEL_INVALID")."<br>";
+echo "SKILL_LEVEL_NOT_SET: " .Messages::getError("SKILL_LEVEL_NOT_SET")."<br>";
+echo "PASSWORD_EMPTY: " .Messages::getError("PASSWORD_EMPTY")."<br>";
+echo "PASSWORD_TOO_SHORT: " .Messages::getError("PASSWORD_TOO_SHORT")."<br>";
+echo "PROFILE_PIC_WRONG_TYPE: " .Messages::getError("PROFILE_PIC_WRONG_TYPE")."<br>";
+echo "HOBBY_DATE_EMPTY: " .Messages::getError("HOBBY_DATE_EMPTY")."<br>";
+echo "HOBBY_DATE_FORMAT_INVALID: " .Messages::getError("HOBBY_DATE_FORMAT_INVALID")."<br>";
+echo "HOBBY_DATE_INVALID: " .Messages::getError("HOBBY_DATE_INVALID")."<br>";
+echo "FAV_COLOR_EMPTY: " .Messages::getError("FAV_COLOR_EMPTY")."<br>";
+echo "FAV_COLOR_INVALID: " .Messages::getError("FAV_COLOR_INVALID")."<br>";
+echo "URL_INVALID: " .Messages::getError("URL_INVALID")."<br>";
+echo "PHONE_NUMBER_INVALID: " .Messages::getError("PHONE_NUMBER_INVALID")."<br>";
 
 ?>
 </body>
