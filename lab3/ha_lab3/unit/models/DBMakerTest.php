2272d6fb10d55c029714b2b9c53647d1e54bbeff
mr-augustine <pushbuttonrobotics@gmail.com>
2015-10-13 11:57:29 -0500

    Fixed UserTest, DBMakerTest

--- lab3/ha_lab3/unit/models/DBMakerTest.php
+++ lab3/ha_lab3/unit/models/DBMakerTest.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'DBMaker.php';
 
 class makeDBTest extends PHPUnit_Framework_TestCase {
 	
-  public function testValidUserCreate() {
+  public function testDatabaseCreate() {
     $myDb = DBMaker::create ('botspacetest');
   }
 }
