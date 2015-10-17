0972a6a80eb1bf66dd606bd855c16bfe9cd8ca50
mr-augustine <pushbuttonrobotics@gmail.com>
2015-10-17 10:26:45 -0500

    Added Skills table

--- lab3/ha_lab3/WebContent/tests/makeDB.php
+++ lab3/ha_lab3/WebContent/tests/makeDB.php
@@ -48,6 +48,33 @@ function makeDB($dbName) {
 		);
 		$st->execute();
 		
+		$st = $db->prepare("DROP TABLE if EXISTS Skills");
+		$st->execute();
+		$st = $db->prepare(
+			"CREATE TABLE Skills (
+  				skillId			 int(3) NOT NULL AUTO_INCREMENT,
+  				skill_name		 varchar(32) UNIQUE NOT NULL COLLATE utf8_unicode_ci,
+  				PRIMARY KEY (skillId)
+			)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"	
+		);
+		$st->execute();
+		
+		$sql = "INSERT INTO Skills (skillId, skill_name) VALUES
+				(:skillId, :skill_name)";
+		$st = $db->prepare($sql);
+		$st->execute (array (':skillId' => 1, ':skill_name' => 'system-design'));
+		$st->execute (array (':skillId' => 2, ':skill_name' => 'programming'));
+		$st->execute (array (':skillId' => 3, ':skill_name' => 'machining'));
+		$st->execute (array (':skillId' => 4, ':skill_name' => 'soldering'));
+		$st->execute (array (':skillId' => 5, ':skill_name' => 'wiring'));
+		$st->execute (array (':skillId' => 6, ':skill_name' => 'circuit-design'));
+		$st->execute (array (':skillId' => 7, ':skill_name' => 'power-systems'));
+		$st->execute (array (':skillId' => 8, ':skill_name' => 'computer-vision'));
+		$st->execute (array (':skillId' => 9, ':skill_name' => 'ultrasonic'));
+		$st->execute (array (':skillId' => 10, ':skill_name' => 'infrared'));
+		$st->execute (array (':skillId' => 11, ':skill_name' => 'GPS'));
+		$st->execute (array (':skillId' => 12, ':skill_name' => 'compase'));
+		
 		$sql = "INSERT INTO Users (userId, email, password) VALUES
 		                          (:userId, :email, :password)";
 		$st = $db->prepare($sql);
@@ -59,7 +86,7 @@ function makeDB($dbName) {
 		$sql = "INSERT INTO UserData (userDataId, userId, user_name, skill_level, profile_pic, started_hobby, fav_color, url, phone) VALUES
 				(:userDataId, :userId, :user_name, :skill_level, :profile_pic, :started_hobby, :fav_color, :url, :phone)";
 		$st = $db->prepare($sql);
-		$st->execute (array (':userDataId' => 1, ':userId' => 1, ':user_name' => 'jabituya', ':skill_level' => 2, ':profile_pic' => 'none.jpg', ':started_hobby' => '2015-10-17 07:38:46', ':fav_color' => '008000', ':url' => 'http://www.google.com', ':phone' => '210-555-9090'));
+		$st->execute (array (':userDataId' => 1, ':userId' => 1, ':user_name' => 'jabituya', ':skill_level' => 2, ':profile_pic' => 'none.jpg', ':started_hobby' => '1986-05-09 00:00:00', ':fav_color' => '008000', ':url' => 'http://www.google.com', ':phone' => '210-555-9090'));
 	} catch ( PDOException $e ) {
 		echo $e->getMessage ();  // not final error handling
 	}
