9968c3c5df620fd6c4f71652839fc6fadce863b7
mr-augustine <pushbuttonrobotics@gmail.com>
2015-10-20 21:14:10 -0500

    Finished prelim SkillsDBTest

--- lab3/ha_lab3/unit/models/UserDataDBTest.php
+++ lab3/ha_lab3/unit/models/UserDataDBTest.php
@@ -4,4 +4,35 @@ require_once dirname(__FILE__).'\..\..\WebContent\models\Messages.class.php';
 require_once dirname(__FILE__).'\..\..\WebContent\models\UserData.class.php';
 require_once dirname(__FILE__).'\..\..\WebContent\models\UserDataDB.class.php';
 require_once dirname(__FILE__).'\..\..\WebContent\tests\DBMaker.php';
+
+class UserDataDBTest extends PHPUnit_Framework_TestCAse {
+	
+	public function testGetAllUserData() {
+		$this->assertTrue(false, 'Test not implmented yet');
+	}
+	
+	public function testValidUserDataCreate() {
+		$this->assertTrue(false, 'Test not implmented yet');
+	}
+	
+	public function testInvalidUserDataCreate() {
+		$this->assertTrue(false, 'Test not implmented yet');
+	}
+	
+	public function testGetUserDataByUserId() {
+		$this->assertTrue(false, 'Test not implmented yet');
+	}
+	
+	public function testGetUserDataByUserName() {
+		$this->assertTrue(false, 'Test not implmented yet');
+	}
+	
+	public function testGetUserDataBySkillLevel() {
+		$this->assertTrue(false, 'Test not implmented yet');
+	}
+	
+	public function testGetUserDataBySkillArea() {
+		$this->assertTrue(false, 'Test not implmented yet');
+	}
+}
 ?>
