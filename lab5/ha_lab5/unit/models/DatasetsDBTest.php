bc8c3cec2255e01fe871fdee6ce5bc6307cc0061
mr-augustine <pushbuttonrobotics@gmail.com>
2015-11-23 09:31:08 -0600

    DatasetsDB unit tests complete

--- lab5/ha_lab5/unit/models/DatasetsDBTest.php
+++ lab5/ha_lab5/unit/models/DatasetsDBTest.php
@@ -8,7 +8,18 @@ require_once dirname(__FILE__).'\..\..\WebContent\models\Dataset.class.php';
 class DatasetsDBTest extends PHPUnit_Framework_TestCase {
 	
 	public function testGetAllDatasets() {
+		$myDb = DBMaker::create('sensordatarepotest');
+		Database::clearDB();
+		$db = Database::getDB('sensordatarepotest', 'C:\xampp\myConfig.ini');
 		
+		$datasets = DatasetsDB::getDatasetsBy();
+		
+		$this->assertEquals(1, count($datasets),
+				'It should fetch all of the datasets in the test database');
+		
+		foreach ($datasets as $dataset)
+			$this->assertTrue(is_a($dataset, 'Dataset'),
+					'It should return valid Dataset objects');
 	}
 	
 	public function testInsertValidDataset() {
@@ -32,27 +43,120 @@ class DatasetsDBTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	public function testInsertInvalidDataset() {
+		$myDb = DBMaker::create('sensordatarepotest');
+		Database::clearDB();
+		$db = Database::getDB('sensordatarepotest', 'C:\xampp\myConfig.ini');
 		
+		$invalidTest = array('dataset_name' => '',
+				'description' => 'A walk in the park with my robot');
+		$invalidDataset = new Dataset($invalidTest);
+		
+		$beforeCount = count(DatasetsDB::getDatasetsBy());
+		$newDataset = DatasetsDB::addDataset($invalidDataset);
+		
+		$this->assertGreaterThan(0, $newDataset->getErrorCount(),
+				'The inserted dataset should return with an error');
+		
+		$afterCount = count(DatasetsDB::getDatasetsBy());
+		$this->assertEquals($afterCount, $beforeCount,
+				'The database should have no additional datasets after insertion');
 	}
 	
 	public function testInsertDuplicateDataset() {
+		$myDb = DBMaker::create('sensordatarepotest');
+		Database::clearDB();
+		$db = Database::getDB('sensordatarepotest', 'C:\xampp\myConfig.ini');
 		
+		$duplicateTest = array('dataset_name' => 'Lincoln Park Run');
+		$duplicateDataset = new Dataset($duplicateTest);
+		
+		$beforeCount = count(DatasetsDB::getDatasetsBy());
+		$newDataset = DatasetsDB::addDataset($duplicateDataset);
+		
+		$this->assertGreaterThan(0, $newDataset->getErrorCount(),
+				'The inserted dataset should return with an error');
+		
+		$afterCount = count(DatasetsDB::getDatasetsBy());
+		$this->assertEquals($afterCount, $beforeCount,
+				'The database should have no additional datasets after insertion');
 	}
 	
 	public function testGetDatasetByDatasetId() {
+		$myDb = DBMaker::create('sensordatarepotest');
+		Database::clearDB();
+		$db = Database::getDB('sensordatarepotest', 'C:\xampp\myConfig.ini');
+		$testDatasetId = 1;
+		$datasets = DatasetsDB::getDatasetsBy('dataset_id', $testDatasetId);
 		
+		$this->assertEquals(1, count($datasets),
+				'The database should return exactly one dataset with the provided id');
+		
+		$dataset = $datasets[0];
+		$this->assertEquals($testDatasetId, $dataset->getDatasetId(),
+				'The database should return the dataset with the provided id');
 	}
 	
 	public function testGetDatasetByDatasetName() {
+		$myDb = DBMaker::create('sensordatarepotest');
+		Database::clearDB();
+		$db = Database::getDB('sensordatarepotest', 'C:\xampp\myConfig.ini');
+		$testDatasetName = 'Lincoln Park Run';
+		$datasets = DatasetsDB::getDatasetsBy('dataset_name', $testDatasetName);
 		
+		$this->assertEquals(1, count($datasets),
+				'The database should return exactly one dataset with the provided name');
+		
+		$dataset = $datasets[0];
+		$this->assertEquals($testDatasetName, $dataset->getDatasetName(),
+				'The database should return the dataset with the provided name');
 	}
 	
 	public function testUpdateDatasetName() {
+		$myDb = DBMaker::create('sensordatarepotest');
+		Database::clearDB();
+		$db = Database::getDB('sensordatarepotest', 'C:\xampp\myConfig.ini');
+		$testDatasetId = 1;
 		
+		$datasets = DatasetsDB::getDatasetsBy('dataset_id', $testDatasetId);
+		$dataset = $datasets[0];
+		
+		$this->assertEquals($dataset->getDatasetName(), 'Lincoln Park Run',
+				'Before the update, it should have email Lincoln Park Run');
+		
+		$params = $dataset->getParameters();
+		$params['dataset_name'] = 'Updated Lincoln Park Run';
+		$newDataset = new Dataset($params);
+		$newDataset->setDatasetId($testDatasetId);
+		
+		$returnedDataset = DatasetsDB::updateDataset($newDataset);
+		
+		$this->assertEquals($returnedDataset->getDatasetName(), $params['dataset_name'],
+				'After the update it should have the name '.$params['dataset_name']);
+		$this->assertTrue(empty($returnedDataset->getErrors()),
+				'The updated dataset should have no errors');
 	}
 	
 	public function testUpdateDatasetDescription() {
+		$myDb = DBMaker::create('sensordatarepotest');
+		Database::clearDB();
+		$db = Database::getDB('sensordatarepotest', 'C:\xampp\myConfig.ini');
+		$testDatasetId = 1;
 		
+		$datasets = DatasetsDB::getDatasetsBy('dataset_id', $testDatasetId);
+		$dataset = $datasets[0];
+		
+		$this->assertTrue(empty($dataset->getDescription()),
+				'Before the update, it should have an empty description');
+		
+		$params = $dataset->getParameters();
+		$params['description'] = 'Updated description';
+		$newDataset = new Dataset($params);
+		$newDataset->setDatasetId($testDatasetId);
+		
+		$returnedDataset = DatasetsDB::updateDataset($newDataset);
+		
+		$this->assertEquals($returnedDataset->getDescription(), $params['description'],
+				'After the update it should have the name '.$params['description']);
 	}
 }
 ?>
