b634f999ccc93ae2491e029a205c2ab64a0fc839
mr-augustine <pushbuttonrobotics@gmail.com>
2015-11-27 22:24:59 -0600

    Added the site logo

--- lab5/ha_lab5/WebContent/views/HomeView.class.php
+++ lab5/ha_lab5/WebContent/views/HomeView.class.php
@@ -19,6 +19,7 @@ class HomeView {
 		echo '<div class="jumbotron">';
 		echo '<div class="container">';
 		
+		echo '<img class="logo" src="/'.$base.'/resources/images/sensor-data-repo-logo.png" alt="sensor data repo logo">';
 		echo '<br><br>';
 		echo '<p><strong>Sensor Data Repo</strong> is the free, simple, and structured way to share your sensor data with a community of peers.</p>';
 		echo '</div>';
